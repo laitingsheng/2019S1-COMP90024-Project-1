8aaea60d661c4fbd2365081f88feed6b2687ab86
Tinson <laitingsheng@vip.qq.com>
2019-03-20 18:43:24 +1100

    Initialise code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,22 @@
-#include <iostream>
+#include <cstdlib>
+
 #include "omp.h"
 
-int main()
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/json_parser.hpp>
+
+using boost::property_tree::ptree;
+using boost::property_tree::json_parser::read_json;
+
+int main(int argc, char * argv[])
 {
+    if (argc < 3)
+    {
+        printf("2 parameter is expected, %d was provided", argc - 1);
+        return -1;
+    }
+
+    ptree grid;
+    read_json(argv[1], grid);
     return 0;
 }
