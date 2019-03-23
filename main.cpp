9661e80400e9c6645f403057f2b9cf2ea94767c2
Tinson <laitingsheng@vip.qq.com>
2019-03-24 02:00:04 +1100

    Completed coordinates parsing

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <list>
 #include <map>
+#include <regex>
 #include <set>
 #include <sstream>
 #include <unordered_map>
@@ -14,40 +16,19 @@
 #include <omp.h>
 
 #include <boost/algorithm/string.hpp>
+#include <boost/mpi.hpp>
 #include <boost/property_tree/json_parser.hpp>
 #include <boost/sort/sort.hpp>
 
 using namespace std;
 using namespace chrono;
 
+namespace mpi = boost::mpi;
+
 using boost::algorithm::to_lower;
 using boost::property_tree::ptree;
 using boost::sort::block_indirect_sort;
 using boost::property_tree::json_parser::read_json;
-
-struct info_counter final : unordered_map<string, pair<unsigned long, unordered_map<string, unsigned long>>>
-{
-    auto merge(info_counter & source) -> void
-    {
-        // @formatter:off
-        for (auto & [k, rv] : source)
-        // @formatter:on
-            if (count(k))
-            {
-                // @formatter:off
-                auto & [lc, lhc] = this->operator[](k);
-                auto & [rc, rhc] = rv;
-                // @formatter:on
-                lc += rc;
-                // @formatter:off
-                for (auto & [h, hc] : rhc)
-                // @formatter:on
-                    lhc[h] += hc;
-            }
-            else
-                this->operator[](k) = move(rv);
-    }
-};
 
 using hash_tag_pair = pair<string, unsigned long>;
 static constexpr auto htp_less(hash_tag_pair const & l, hash_tag_pair const & r) -> bool
@@ -73,19 +54,14 @@ static constexpr auto ct_less(cell_tuple const & l, cell_tuple const & r) -> boo
 
 int main(int argc, char * argv[])
 {
-    auto start = system_clock::now();
-
     if (argc < 2)
     {
-        // @formatter:off
-        printf(
-            "time used: %.3lfs\n",
-            duration_cast<milliseconds>(system_clock::now() - start).count() * 1.0 / 1000
-        );
-        // @formatter:on
         printf("1 parameter is expected, %d was provided", argc - 1);
         return -1;
     }
+
+    mpi::environment env(argc, argv);
+    mpi::communicator world;
 
     // hard-coded grid, to eliminate time for reading the melbGrid.json
     // @formatter:off
@@ -106,180 +82,83 @@ int main(int argc, char * argv[])
         {-37.95, 'D'},
         {-38.1, 'D'}
     };
-    // @formatter:on
     unordered_set<string> const invalid {"A5", "B5", "D1", "D2"};
+    regex const coord_rgx {R"("coordinates":\[(-?\d*(?:\.\d*)?),(-?\d*(?:\.\d*)?)\])"},
+                hash_tag_rgx {R"("hashtags":\[((?:\{.*"text":"[[:alnum:]]+".*\})+)\])"};
+    // @formatter:on
 
     // ignore the first line of the data file
+    auto start = system_clock::now();
     ifstream twit_file(argv[1]);
     twit_file.ignore(numeric_limits<streamsize>::max(), '\n');
-    vector<info_counter> counters(omp_get_num_procs());
+    unordered_map<string, list<ptree>> records;
 
     // process the file in parallel
     #pragma omp parallel
+    while (true)
     {
-        auto & counter = counters[omp_get_thread_num()];
-        while (true)
+        // attempt to read the file, or terminate current thread if the file reaches the end
+        string post_info;
+        auto finished = false;
+        #pragma omp critical
         {
-            // attempt to read the file, or terminate current thread if the file reaches the end
-            string post_str;
-            auto finished = false;
-            #pragma omp critical
-            {
-                if (twit_file.eof())
-                    finished = true;
-                else
-                    getline(twit_file, post_str);
-            };
-            if (finished || post_str.empty() || post_str == "]}")
-                break;
-
-            // trim the trailing comma if it appears and parse the string
-            auto post_end = post_str.end() - 2;
-            if (*post_end == ',')
-                post_str.erase(post_end, post_end + 2);
+            if (twit_file.eof())
+                finished = true;
             else
-                post_str.pop_back();
-            ptree post;
-            stringstream _0(post_str);
-            read_json(_0, post);
+                getline(twit_file, post_info);
+        };
+        if (finished)
+            break;
 
-            // get information for the JSON
-            // @formatter:off
-            auto const & doc = post.get_child("doc"),
-                       & coordinates = doc.get_child("coordinates");
-            // @formatter:on
-            if (coordinates.empty())
+        smatch coord_search;
+        if (!regex_search(post_info, coord_search, coord_rgx))
+            continue;
+
+        auto longitude = stod(coord_search[1].str());
+        auto hlbit = horizontal.lower_bound(longitude);
+        auto hlbv = hlbit->second;
+        if (!hlbv)
+            continue;
+        if (longitude < hlbit->first)
+        {
+            if (hlbit == horizontal.begin())
                 continue;
-
-            // determine the longitude area
-            auto citer = coordinates.get_child("coordinates").begin();
-            ptree const & long_ptree = citer->second;
-            char hc;
-            auto longitude = stod(long_ptree.data());
-            auto hlbit = horizontal.lower_bound(longitude);
-            auto hlbv = hlbit->second;
-            if (!hlbv)
-                continue;
-            if (longitude < hlbit->first)
-            {
-                if (hlbit == horizontal.begin())
-                    continue;
-                hlbv = (--hlbit)->second;
-            }
-            hc = hlbv;
-
-            // determine the latitude area
-            ptree const & lat_ptree = (++citer)->second;
-            char vc;
-            auto latitude = stod(lat_ptree.data());
-            auto vlbit = vertical.lower_bound(latitude);
-            auto vlbv = vlbit->second;
-            if (!vlbv)
-                continue;
-            if (latitude < vlbit->first && vlbit == vertical.begin())
-                continue;
-            vc = vlbv;
-
-            // determine if it is in the valid region
-            string coord_desc {vc, hc};
-            if (invalid.count(coord_desc))
-                continue;
-
-            // increment total count
-            // @formatter:off
-            auto & [count, hashtags_count] = counter[move(coord_desc)];
-            // @formatter:on
-            ++count;
-
-            // parse hash tags information
-            // @formatter:off
-            for (auto const & [_, tag] : doc.get_child("entities").get_child("hashtags"))
-            // @formatter:on
-            {
-                string text = tag.get_child("text").data();
-                to_lower(text);
-                ++hashtags_count[move(text)];
-            }
+            hlbv = (--hlbit)->second;
         }
-    };
+        auto hc = hlbv;
+
+        auto latitude = stod(coord_search[2].str());
+        auto vlbit = vertical.lower_bound(latitude);
+        auto vlbv = vlbit->second;
+        if (!vlbv)
+            continue;
+        if (latitude < vlbit->first && vlbit == vertical.begin())
+            continue;
+        auto vc = vlbv;
+
+        string coord_desc {vc, hc};
+        if (invalid.count(coord_desc))
+            continue;
+
+        printf("%3d: %s\n", omp_get_thread_num(), coord_desc.c_str());
+    }
 
     // @formatter:off
     printf(
-        "I/O time used: %.3lfs\n",
+        "%d out of %d: I/O time used: %.3lfs\n", world.rank() + 1, world.size(),
         duration_cast<milliseconds>(system_clock::now() - start).count() * 1.0 / 1000
     );
     // @formatter:on
     start = system_clock::now();
 
-    // logarithmic merging
-    auto pc = omp_get_num_procs() >> 1;
-    while (pc > 1)
-    {
-        #pragma omp parallel for num_threads(pc)
-        for (int i = 0; i < pc; ++i)
-            counters[i].merge(counters[i + pc]);
-        pc >>= 1;
-    }
-    // final merge
-    counters[0].merge(counters[1]);
-    auto const & counter = counters[0];
-
-    vector<cell_tuple> output(counter.size());
-    auto cit = counter.begin(), cend = counter.end();
-    auto vit = output.begin();
-    #pragma omp parallel
-    {
-        while (true)
-        {
-            auto finished = false;
-            decltype(cit) ccit;
-            decltype(vit) cvit;
-            #pragma omp critical
-            {
-                if (cit == cend)
-                    finished = true;
-                else
-                {
-                    ccit = cit++;
-                    cvit = vit++;
-                }
-            };
-            if (finished)
-                break;
-
-            auto const & [k, v] = *ccit;
-            auto const & [c, hm] = v;
-
-            vector<hash_tag_pair> tmp(hm.begin(), hm.end());
-            sort(tmp.begin(), tmp.end(), htp_less);
-            if (tmp.size() > 5)
-                tmp.erase(tmp.begin() + 5, tmp.end());
-            *cvit = {k, c, move(tmp)};
-        }
-    };
-    block_indirect_sort(output.begin(), output.end(), ct_less);
+    // process
 
     // @formatter:off
     printf(
-        "processing time used: %.3lfs\n\n",
+        "%d out of %d: processing time used: %.3lfs\n", world.rank() + 1, world.size(),
         duration_cast<milliseconds>(system_clock::now() - start).count() * 1.0 / 1000
     );
     // @formatter:on
-
-    for (auto const & [k, c, _] : output)
-        printf("%s: %lu\n", k.c_str(), c);
-    for (auto const & [k, _, htp] : output)
-    {
-        printf("%s: (", k.c_str());
-        auto it = htp.begin();
-        for (; it != htp.end() - 1; ++it)
-        {
-            auto & [t, c] = *it;
-            printf("(#%s, %lu)", t.c_str(), c);
-        }
-        auto & [t, c] = *it;
-        printf("(#%s, %lu))\n", t.c_str(), c);
-    }
 
     return 0;
 }
