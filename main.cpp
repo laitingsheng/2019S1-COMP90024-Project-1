ae1ed7ddeb5713f1aeb781d6257d0045b8061d72
Tinson Lai <laitinson@gmail.com>
2019-03-25 02:32:50 +1100

    Parallelise file reading

--- main.cpp
+++ main.cpp
@@ -28,6 +28,8 @@ using namespace filesystem;
 namespace mpi = boost::mpi;
 
 using boost::algorithm::to_lower;
+using boost::property_tree::ptree;
+using boost::property_tree::json_parser::read_json;
 // @formatter:off
 
 int main(int argc, char * argv[])
@@ -72,23 +74,25 @@ int main(int argc, char * argv[])
                 hash_tags_rgx(R"("hashtags":\[(\{(?:"indices":\[\d+,\d+\],)?"text":"\w+"(?:,"indices":\[(?:\d+(?:,\d+)*)?\])?\}(?:,\{(?:"indices":\[\d+,\d+\],)?"text":"\w+"(?:,"indices":\[(?:\d+(?:,\d+)*)?\])?\})*)\])");
     // @formatter:on
 
-    // ignore the first line of the data file
+    // record time used
     auto start = system_clock::now();
-    unordered_map<string, pair<unsigned long, list<string>>> records;
 
     // process the file in parallel
-    auto num_procs = omp_get_num_procs();
-    auto read_block_size = file_size(input_file_path) / num_procs;
-    vector<unsigned long> file_starts(num_procs);
+    auto const num_procs = omp_get_num_procs();
+    auto const read_block_size = file_size(input_file_path) / num_procs;
+    vector<streamsize> read_starts(num_procs);
+    unordered_map<string, pair<unsigned long, list<string>>> records;
     #pragma omp parallel
     {
-        auto tn = omp_get_thread_num();
+        auto const tn = omp_get_thread_num();
 
         ifstream twit_file(argv[1]);
-        auto file_start = read_block_size * tn;
-        twit_file.seekg(file_start);
-        file_starts[tn] = file_start;
-
+        auto const read_start = read_block_size * tn;
+        twit_file.seekg(read_start);
+        if (tn > 0)
+            // set the offset to the start of next line
+            twit_file.ignore(numeric_limits<streamsize>::max(), '\n');
+        read_starts[tn] = twit_file.tellg();
         #pragma omp barrier
 
         while (true)
@@ -96,7 +100,14 @@ int main(int argc, char * argv[])
             // attempt to read the file, or terminate current thread if the file reaches the end
             string post_info;
             auto finished = false;
-            #pragma omp critical
+            if (tn < num_procs - 1)
+            {
+                if (twit_file.tellg() == read_starts[tn + 1])
+                    finished = true;
+                else
+                    getline(twit_file, post_info);
+            }
+            else
             {
                 if (twit_file.eof())
                     finished = true;
@@ -106,43 +117,44 @@ int main(int argc, char * argv[])
             if (finished)
                 break;
 
-            // smatch coord_search;
-            // if (!regex_search(post_info, coord_search, coord_rgx))
-            //     continue;
-            //
-            // auto longitude = stod(coord_search[1].str());
-            // auto hlbit = horizontal.lower_bound(longitude);
-            // auto hlbv = hlbit->second;
-            // if (!hlbv)
-            //     continue;
-            // if (longitude < hlbit->first)
-            // {
-            //     if (hlbit == horizontal.begin())
-            //         continue;
-            //     hlbv = (--hlbit)->second;
-            // }
-            // auto hc = hlbv;
-            //
-            // auto latitude = stod(coord_search[2].str());
-            // auto vlbit = vertical.lower_bound(latitude);
-            // auto vlbv = vlbit->second;
-            // if (!vlbv)
-            //     continue;
-            // if (latitude < vlbit->first && vlbit == vertical.begin())
-            //     continue;
-            // auto vc = vlbv;
-            //
-            // string coord_desc {vc, hc};
-            // if (invalid.count(coord_desc))
-            //     continue;
 
-            // auto &[count, hash_tags_str] = records[move(coord_desc)];
-            // ++count;
+            smatch coord_search;
+            if (!regex_search(post_info, coord_search, coord_rgx))
+                continue;
 
-            // smatch tags_search;
-            // if (!regex_search(post_info, tags_search, hash_tags_rgx))
-            //     continue;
-            // hash_tags_str.emplace_back(tags_search[1].str());
+            auto longitude = stod(coord_search[1].str());
+            auto hlbit = horizontal.lower_bound(longitude);
+            auto hlbv = hlbit->second;
+            if (!hlbv)
+                continue;
+            if (longitude < hlbit->first)
+            {
+                if (hlbit == horizontal.begin())
+                    continue;
+                hlbv = (--hlbit)->second;
+            }
+            auto hc = hlbv;
+
+            auto latitude = stod(coord_search[2].str());
+            auto vlbit = vertical.lower_bound(latitude);
+            auto vlbv = vlbit->second;
+            if (!vlbv)
+                continue;
+            if (latitude < vlbit->first && vlbit == vertical.begin())
+                continue;
+            auto vc = vlbv;
+
+            string coord_desc {vc, hc};
+            if (invalid.count(coord_desc))
+                continue;
+
+            auto &[count, hash_tags_str] = records[move(coord_desc)];
+            ++count;
+
+            smatch tags_search;
+            if (!regex_search(post_info, tags_search, hash_tags_rgx))
+                continue;
+            hash_tags_str.emplace_back(tags_search[1].str());
         }
     }
 
