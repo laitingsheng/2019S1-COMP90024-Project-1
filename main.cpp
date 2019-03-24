feadd3f7ee6815ac9f4d0b6eed527dc463a32990
Tinson <laitingsheng@vip.qq.com>
2019-03-24 17:29:41 +1100

    Add regex for hash tags strings & start parallelising file reading

--- main.cpp
+++ main.cpp
@@ -2,18 +2,19 @@
  * Author: Tingsheng (Tinson) Lai 781319
  */
 
+// @formatter:off
+#include <omp.h>
+#include <mpi.h>
+
 #include <chrono>
+#include <filesystem>
 #include <fstream>
-#include <iostream>
 #include <list>
 #include <map>
 #include <regex>
 #include <set>
-#include <sstream>
 #include <unordered_map>
 #include <unordered_set>
-
-#include <omp.h>
 
 #include <boost/algorithm/string.hpp>
 #include <boost/mpi.hpp>
@@ -22,46 +23,30 @@
 
 using namespace std;
 using namespace chrono;
+using namespace filesystem;
 
 namespace mpi = boost::mpi;
 
 using boost::algorithm::to_lower;
-using boost::property_tree::ptree;
-using boost::sort::block_indirect_sort;
-using boost::property_tree::json_parser::read_json;
-
-using hash_tag_pair = pair<string, unsigned long>;
-static constexpr auto htp_less(hash_tag_pair const & l, hash_tag_pair const & r) -> bool
-{
-    auto ls = l.second, rs = r.second;
-    if (ls < rs)
-        return false;
-    if (ls == rs)
-        return l.first < r.first;
-    return true;
-}
-
-using cell_tuple = tuple<string, unsigned long, vector<hash_tag_pair>>;
-static constexpr auto ct_less(cell_tuple const & l, cell_tuple const & r) -> bool
-{
-    auto ls = get<1>(l), rs = get<1>(r);
-    if (ls < rs)
-        return false;
-    if (ls == rs)
-        return get<0>(l) < get<0>(r);
-    return true;
-}
+// @formatter:off
 
 int main(int argc, char * argv[])
 {
     if (argc < 2)
     {
-        printf("1 parameter is expected, %d was provided", argc - 1);
+        fprintf(stderr, "1 parameter is expected, %d was provided", argc - 1);
         return -1;
     }
 
     mpi::environment env(argc, argv);
     mpi::communicator world;
+
+    path const input_file_path(argv[1]);
+    if (!is_regular_file(input_file_path))
+    {
+        fprintf(stderr, "The given file path %s is not a regular file\n", argv[1]);
+        return -1;
+    }
 
     // hard-coded grid, to eliminate time for reading the melbGrid.json
     // @formatter:off
@@ -83,64 +68,82 @@ int main(int argc, char * argv[])
         {-38.1, 'D'}
     };
     unordered_set<string> const invalid {"A5", "B5", "D1", "D2"};
-    regex const coord_rgx {R"("coordinates":\[(-?\d*(?:\.\d*)?),(-?\d*(?:\.\d*)?)\])"},
-                hash_tag_rgx {R"("hashtags":\[((?:\{.*"text":"[[:alnum:]]+".*\})+)\])"};
+    regex const coord_rgx(R"("coordinates":\[(-?\d*(?:\.\d*)?),(-?\d*(?:\.\d*)?)\])"),
+                hash_tags_rgx(R"("hashtags":\[(\{(?:"indices":\[\d+,\d+\],)?"text":"\w+"(?:,"indices":\[(?:\d+(?:,\d+)*)?\])?\}(?:,\{(?:"indices":\[\d+,\d+\],)?"text":"\w+"(?:,"indices":\[(?:\d+(?:,\d+)*)?\])?\})*)\])");
     // @formatter:on
 
     // ignore the first line of the data file
     auto start = system_clock::now();
-    ifstream twit_file(argv[1]);
-    twit_file.ignore(numeric_limits<streamsize>::max(), '\n');
-    unordered_map<string, list<ptree>> records;
+    unordered_map<string, pair<unsigned long, list<string>>> records;
 
     // process the file in parallel
+    auto num_procs = omp_get_num_procs();
+    auto read_block_size = file_size(input_file_path) / num_procs;
+    vector<unsigned long> file_starts(num_procs);
     #pragma omp parallel
-    while (true)
     {
-        // attempt to read the file, or terminate current thread if the file reaches the end
-        string post_info;
-        auto finished = false;
-        #pragma omp critical
-        {
-            if (twit_file.eof())
-                finished = true;
-            else
-                getline(twit_file, post_info);
-        };
-        if (finished)
-            break;
+        auto tn = omp_get_thread_num();
 
-        smatch coord_search;
-        if (!regex_search(post_info, coord_search, coord_rgx))
-            continue;
+        ifstream twit_file(argv[1]);
+        auto file_start = read_block_size * tn;
+        twit_file.seekg(file_start);
+        file_starts[tn] = file_start;
 
-        auto longitude = stod(coord_search[1].str());
-        auto hlbit = horizontal.lower_bound(longitude);
-        auto hlbv = hlbit->second;
-        if (!hlbv)
-            continue;
-        if (longitude < hlbit->first)
+        #pragma omp barrier
+
+        while (true)
         {
-            if (hlbit == horizontal.begin())
-                continue;
-            hlbv = (--hlbit)->second;
+            // attempt to read the file, or terminate current thread if the file reaches the end
+            string post_info;
+            auto finished = false;
+            #pragma omp critical
+            {
+                if (twit_file.eof())
+                    finished = true;
+                else
+                    getline(twit_file, post_info);
+            };
+            if (finished)
+                break;
+
+            // smatch coord_search;
+            // if (!regex_search(post_info, coord_search, coord_rgx))
+            //     continue;
+            //
+            // auto longitude = stod(coord_search[1].str());
+            // auto hlbit = horizontal.lower_bound(longitude);
+            // auto hlbv = hlbit->second;
+            // if (!hlbv)
+            //     continue;
+            // if (longitude < hlbit->first)
+            // {
+            //     if (hlbit == horizontal.begin())
+            //         continue;
+            //     hlbv = (--hlbit)->second;
+            // }
+            // auto hc = hlbv;
+            //
+            // auto latitude = stod(coord_search[2].str());
+            // auto vlbit = vertical.lower_bound(latitude);
+            // auto vlbv = vlbit->second;
+            // if (!vlbv)
+            //     continue;
+            // if (latitude < vlbit->first && vlbit == vertical.begin())
+            //     continue;
+            // auto vc = vlbv;
+            //
+            // string coord_desc {vc, hc};
+            // if (invalid.count(coord_desc))
+            //     continue;
+
+            // auto &[count, hash_tags_str] = records[move(coord_desc)];
+            // ++count;
+
+            // smatch tags_search;
+            // if (!regex_search(post_info, tags_search, hash_tags_rgx))
+            //     continue;
+            // hash_tags_str.emplace_back(tags_search[1].str());
         }
-        auto hc = hlbv;
-
-        auto latitude = stod(coord_search[2].str());
-        auto vlbit = vertical.lower_bound(latitude);
-        auto vlbv = vlbit->second;
-        if (!vlbv)
-            continue;
-        if (latitude < vlbit->first && vlbit == vertical.begin())
-            continue;
-        auto vc = vlbv;
-
-        string coord_desc {vc, hc};
-        if (invalid.count(coord_desc))
-            continue;
-
-        printf("%3d: %s\n", omp_get_thread_num(), coord_desc.c_str());
     }
 
     // @formatter:off
