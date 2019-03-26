d795c0dd9091090cdd2f8534a62d3feca7cbf381
Tinson <laitingsheng@vip.qq.com>
2019-03-27 10:16:19 +1100

    Add single thread implementation

--- src/process.hpp
+++ src/process.hpp
@@ -3,7 +3,10 @@
 
 #include <sys/stat.h>
 
+#include <fstream>
 #include <regex>
+#include <unordered_map>
+#include <utility>
 
 struct processor
 {
@@ -11,14 +14,27 @@ struct processor
 
     virtual result_type operator()() = 0;
 protected:
+    // @formatter:off
+    using record_type = std::unordered_map<
+        std::string,
+        std::pair<
+            unsigned long,
+            std::unordered_map<std::string, unsigned long>
+        >
+    >;
+    // @formatter:on
+
     static std::regex coord_rgx, hash_tags_rgx, hash_tag_rgx;
     std::string filename;
     struct stat st;
+    record_type record;
 
     explicit processor(char const * filename) : filename(filename)
     {
         stat(filename, &st);
     }
+private:
+    virtual void preprocess() = 0;
 };
 
 std::regex processor::coord_rgx(R"cr("coordinates":\[(.+?),(.+?)\])cr");
@@ -32,6 +48,46 @@ struct single_thread_processor final : public processor
     result_type operator()() override
     {
         return {};
+    };
+private:
+    void preprocess() override
+    {
+        std::ifstream twit_file(filename);
+        while (!twit_file.eof())
+        {
+            std::string buff;
+            std::getline(twit_file, buff);
+
+            std::smatch coord_search;
+            if (!std::regex_search(buff, coord_search, coord_rgx))
+                continue;
+
+            auto longitude = stod(coord_search[1].str());
+            auto hlbit = horizontal.lower_bound(longitude);
+            auto hlbv = hlbit->second;
+            if (!hlbv)
+                continue;
+            if (longitude < hlbit->first)
+            {
+                if (hlbit == horizontal.begin())
+                    continue;
+                hlbv = (--hlbit)->second;
+            }
+            auto hc = hlbv;
+
+            auto latitude = stod(coord_search[2].str());
+            auto vlbit = vertical.lower_bound(latitude);
+            auto vlbv = vlbit->second;
+            if (!vlbv)
+                continue;
+            if (latitude < vlbit->first && vlbit == vertical.begin())
+                continue;
+            auto vc = vlbv;
+
+            std::string coord_desc {vc, hc};
+            if (!valid.count(coord_desc))
+                continue;
+        }
     }
 };
 
