cde5c68b1c81f7323e99f8f69c915166ec515ff3
Mofan Li <li_mofan@qq.com>
2019-04-10 18:17:20 +0800

    change file reading method
    improve efficiency and fix bugs

--- python/main.py
+++ python/main.py
@@ -1,51 +1,62 @@
 from multiprocessing import cpu_count, Pool
 from sys import argv
 from collections import defaultdict as dd
-import ijson
+import re
 
-TFPATH = "tinyTwitter.json"
-X_MIN = 144.700000
-Y_MIN = -37.650000
+TFPATH = "D:/comp90024-project-1/tinyTwitter.json"
+X_MIN = 144.7
+Y_MIN = -37.65
 GRID_WIDTH = 0.15
-DEFAULT_CORE_NUM = 4
+TWITTER_TEMPLATE = '''^{"id":".*,"value":{.*"geometry":{".*"coordinates":\[(.*),(.*)]}.*"doc":{.*"entities":{.*"hashtags":\[(\{.*?\})?]'''
 
-def process_twit(twit):
-	x_coor, y_coor = twit["coordinates"]
-	horizontal_grid = chr(ord('1') + (x_coor - X_MIN) // GRID_WIDTH)
-	vertical_grid = chr(ord('A') + (y_coor - Y_MIN) // GRID_WIDTH)
-	grid_name = horizontal_grid + vertical_grid
-	hash_tags = [ht["text"] for ht in twit["doc"]["entities"]["hashtags"]]
-	return grid_name, hash_tags
+def process_line(line):
+	data_filter = re.compile(TWITTER_TEMPLATE)
+	result = data_filter.match(line)
+
+	if result is None:
+		return "", {}
+
+	x_coor, y_coor, hashtags_data = float(result.groups()[0]), float(result.groups()[1]), result.groups()[2]
+
+	horizontal_grid = chr(int(ord('1') + (x_coor - X_MIN) // GRID_WIDTH))
+	vertical_grid = chr(int(ord('A') + (Y_MIN - y_coor) // GRID_WIDTH))
+	grid_name = vertical_grid + horizontal_grid
+
+	hashtag_filter = re.compile(r'"text":"(.*?)"')
+	if hashtags_data is not None:
+		hashtags = [ht.lower() for ht in hashtag_filter.findall(hashtags_data)]
+	else:
+		hashtags = []
+	return grid_name, hashtags
 
 def processTwitterFile(data):
-	file, start_point, length = data
-	twits = ijson.items(file, 'rows.item')
-
-	for i in range(start_point):
-		next(twits)
-
+	file, start_point, end_point = open(TFPATH, "r", encoding='UTF-8'), data[0], data[1]	
 	grid_twit_count = dd(int)
 	grid_hashtag_count = dd(dict)
-	for i in range(length):
-		grid, hashtags = process_twit(next(twits))
-		grid_twit_count[grid] += 1
-		for hashtag in hashtags:
-			if hashtag in  grid_hashtag_count[grid]:
-				grid_hashtag_count[grid][hashtag] += 1
-			else:
-				grid_hashtag_count[grid][hashtag] = 1
+	file.seek(start_point)
 
-	return grid_twit_count, grid_hashtag_count
+	file.readline()
+	while file.tell() < end_point:
+		new_line = file.readline()
+		grid_name, hashtags = process_line(new_line)
+		if not grid_name:
+			continue
+
+		grid_twit_count[grid_name] += 1
+		for hashtag in hashtags:
+			if hashtag in  grid_hashtag_count[grid_name]:
+				grid_hashtag_count[grid_name][hashtag] += 1
+			else:
+				grid_hashtag_count[grid_name][hashtag] = 1
+	return dict(grid_twit_count), dict(grid_hashtag_count)
 
 if __name__ == '__main__':
 	file = open(TFPATH, "r", encoding='UTF-8')
-	twits = ijson.items(file, 'rows.item')
-	num_of_twit = 0
-	for i in twits:
-		num_of_twit += 1
+	file_size = file.seek(0, 2)
+	core_num = cpu_count()
+	file_length_per_core = file_size//core_num
 
-	process_list = [(file, num_of_twit // DEFAULT_CORE_NUM * i, 
-					min(num_of_twit // DEFAULT_CORE_NUM * (i + 1), num_of_twit) - num_of_twit // DEFAULT_CORE_NUM * i) for i in range(num_of_twit)]
+	process_list = [(file_length_per_core * i, file_length_per_core * (i + 1)) for i in range(core_num)]
 
 	pool = Pool()
 	results = pool.map(processTwitterFile, process_list)
@@ -65,10 +76,15 @@ if __name__ == '__main__':
 				else:
 					grid_hashtag_count[grid][hashtag] = ghc_d[grid][hashtag]
 
-	twitter_count_order = sorted(grid_twit_count.items(), lambda x: x[1], reverse = True)
+	twitter_count_order = sorted(grid_twit_count.items(), key = lambda x: x[1], reverse = True)
+
+	print("Twitter posts number ranking:")
 	for grid in twitter_count_order:
 		print(grid[0] + ": " + str(grid[1]) + " posts, ")
 
+	print("Top 5 hashtags in the area:")
 	for grid in grid_hashtag_count:
-		top5hashtags = sorted(grid_hashtag_count[grid].items(), lambda x: x[1], reverse = True)[:5]
+		top5hashtags = sorted(grid_hashtag_count[grid].items(), key = lambda x: x[1], reverse = True)[:5]
 		print(grid + ": " + str(top5hashtags))
+
+
