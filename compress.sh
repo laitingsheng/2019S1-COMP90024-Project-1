c94efda08dbc972fbf5437a00b374cb2b229568e
Tinson Lai <laitinson@gmail.com>
2019-04-15 10:29:17 +1000

    Remove generator scripts from the zip file

--- compress.sh
+++ compress.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-zip -9 -ur Tingsheng-Lai-781319.zip main *.slurm build.* gen.* COMP90024\ Project\ 1\ Report.pdf
+zip -9 -ur Tingsheng-Lai-781319.zip main *.slurm build.* COMP90024\ Project\ 1\ Report.pdf
